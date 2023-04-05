b8a3dca754c352765e3c05deaa32868dba537680
= <=>
2023-04-05 10:27:57 +0530

    first commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,24 @@
+<div class = "container">
+   
+    <div class="input-editor">Hello</div>
+
+    <div class="output-editor" >Bro</div>
+</div>
+
+<style>
+    .container {
+        margin: 0px;
+        height: 100vh;
+        display: flex;
+        background-color: yellow;
+    }
+
+    .input-editor{
+        background-color: green;
+        flex: 1 1 auto
+    }
+    .output-editor{
+        background-color: blue;
+        flex: 1 1 auto
+    }
+</style>
